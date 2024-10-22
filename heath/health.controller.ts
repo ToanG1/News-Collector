efe7c36e7475f2ad093f0389db3337ee222db645
ToanG1 <Toandinh01675@gmail.com>
2024-10-22 21:20:57 +0700

    update health api

--- heath/health.controller.ts
+++ heath/health.controller.ts
@@ -1,7 +1,7 @@
 import { api } from "encore.dev/api";
 import { IResponse } from "../common/dto/response.interface";
 
-export const get = api(
+export const checkHealthAPI = api(
   { expose: true, method: "GET", path: "/health" },
   async (): Promise<IResponse> => {
     return { message: "News collector still alive!" };
--- heath/health.test.ts
+++ heath/health.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from "vitest";
+import { describe, test } from "vitest";
 
 describe("ignore", () => {
   test("ignore this test", async () => {});
