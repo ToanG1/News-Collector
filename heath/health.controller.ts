c8aa5693d57b7584991d2f18c1651b81062385f2
ToanG1 <Toandinh01675@gmail.com>
2024-10-22 22:53:04 +0700

    update api check health

--- heath/health.controller.ts
+++ heath/health.controller.ts
@@ -1,9 +1,15 @@
 import { api } from "encore.dev/api";
 import { IResponse } from "../common/dto/response.interface";
+import { SQLDatabase } from "encore.dev/storage/sqldb";
+
+const database = new SQLDatabase("health", { migrations: "./migrations" });
 
 export const checkHealthAPI = api(
   { expose: true, method: "GET", path: "/health" },
   async (): Promise<IResponse> => {
-    return { message: "News collector still alive!" };
+    const time = await database.queryRow`SELECT NOW()`.then((row) => row!.now);
+    return {
+      message: `${time}: News collector still alive!`,
+    };
   }
 );
