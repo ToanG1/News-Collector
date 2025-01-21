f1fd47f5cf03a89361dae4530f406eccbd65eed3
ToanG1 <Toandinh01675@gmail.com>
2025-01-21 23:15:50 +0700

    update

--- news/fetch.util.ts
+++ news/fetch.util.ts
@@ -5,11 +5,11 @@ const api_url = "https://api.scraperapi.com";
 const customizeHeaders = "country_code=vn&device_type=desktop&render=true";
 
 export const fetchByScraperApi = async (url: string) => {
-  const response = await fetch(
-    `${api_url}?api_key=${api_key}&url=${encodeURIComponent(
-      url
-    )}&${customizeHeaders}`
-  );
+  const reqUrl = `${api_url}?api_key=${api_key}&url=${encodeURIComponent(
+    url
+  )}&${customizeHeaders}`;
+
+  const response = await fetch(reqUrl);
   if (response.status !== 200)
     throw new Error(
       `Scraper response failed with code: ${response.status} \n ${response.statusText}`
